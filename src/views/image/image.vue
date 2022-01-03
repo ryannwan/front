48449e6ef2734c82a578a1b144f3037c4dabbc27
31170 <3117046103@qq.com>
2022-01-03 22:19:25 +0000

    wry

--- src/views/image/image.vue
+++ src/views/image/image.vue
@@ -18,9 +18,9 @@
     <div class="app-container">
       <el-button size="small" type="success" @click="upload">确认上传</el-button>
     </div>
-<div>
-  <img :src="picurl" alt="beachball" />
-</div>
+    <div>
+      <img :src="picurl">
+    </div>
   </div>
 </template>
 
@@ -32,7 +32,7 @@ export default {
 
   data() {
     return {
-      picurl: "",
+      picurl: '',
       file: {},
       fileList: []
     }
@@ -56,24 +56,26 @@ export default {
     },
     // ArrayBuffer转为base64字符串
     arrayBufferToBase64(buffer) {
-      //第一步，将ArrayBuffer转为二进制字符串
-      var binary = "";
-      var bytes = new Uint8Array(buffer);
-      var len = bytes.byteLength;
+      // 第一步，将ArrayBuffer转为二进制字符串
+      var binary = ''
+      var bytes = new Uint8Array(buffer)
+      var len = bytes.byteLength
       for (var i = 0; i < len; i++) {
-        binary += String.fromCharCode(bytes[i]);
+        binary += String.fromCharCode(bytes[i])
       }
-      //将二进制字符串转为base64字符串
-      return window.btoa(binary);
+      // 将二进制字符串转为base64字符串
+      return window.btoa(binary)
     },
     upload() {
       const fd = new FormData()
       fd.append('file', this.file)
       axios.post(
         'http://127.0.0.1:5000/image', fd,
-        { headers: { 'Content-Type': 'multipart/form-data' }},
+        { headers: { 'Content-Type': 'multipart/form-data' }, responseType: 'arraybuffer' }
+
       ).then(response => {
-        this.picurl = 'data:image/jpeg;base64,' + this.arrayBufferToBase64(response.data)
+        this.picurl = 'data:image/png;base64,' + this.arrayBufferToBase64(response.data)
+        console.log(this.picurl)
       })
     }
   }
