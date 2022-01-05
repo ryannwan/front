ab62fb9831fb9b015a0a04950e149fa4094e81e0
31170 <3117046103@qq.com>
2022-01-05 11:38:24 +0000

    wry

---
+++ src/views/video/video.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="app-container">
+    <img src="http://127.0.0.1:5000/video_feed" >
+<!--    <el-button size="small" type="success" @click="onClick">按钮</el-button>-->
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+  name: 'Video',
+  data() {
+    return {
+      picurl: ''
+    }
+  },
+  methods: {
+    onClick() {
+      axios.post(
+        'http://127.0.0.1:5000/video_feed'
+      ).then(response => {
+        console.log(this.picurl)
+        this.picurl = response.data
+        console.log(this.picurl)
+      })
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
